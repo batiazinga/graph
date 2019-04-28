2f911c4f6b3f6f5339a45c4b8fade3bcde12bb4e
batiazinga <luc.nguyenthe@gmail.com>
2019-04-28 10:18:28 +0200

    Implement BFS search

--- bfs.go
+++ bfs.go
@@ -35,10 +35,10 @@ type BfsVisitor interface {
 }
 
 // BreadthFirstVisit visits a graph starting from the source vertex
-// and visiting vertices that are closer first.
+// and visiting closer vertices first.
 //
 // At some event points the visitor is called.
-// An appropriate visitor can then compute shortest paths or precedence map.
+// An appropriate visitor can then compute distances and shortest paths.
 func BreadthFirstVisit(g Forward, vis BfsVisitor, source string) {
 	// queue implemented with a list
 	queue := list.New()
@@ -90,7 +90,69 @@ func BreadthFirstVisit(g Forward, vis BfsVisitor, source string) {
 	}
 }
 
-// TODO: Implement a Breadth-First Search which stops once the target has been discovered.
+// BreadthFirstSearch visits a graph starting from the source vertex.
+// It visits closer vertices first and stops when the target is discovered.
+//
+// At some event points the visitor is called.
+// An appropriate visitor can then compute distances and shortest paths.
+//
+// Methods TreeEdge(v, target) and DiscoverVertex(target) are called before the search stops.
+// If the target is not reachable from the source, it is equivalent to BreadthFisrtVisit.
+func BreadthFirstSearch(g Forward, vis BfsVisitor, source, target string) {
+	// queue implemented with a list
+	queue := list.New()
+	// init color map
+	cmap := make(colorMap)
+
+	// discover the source vertex:
+	// it was white, it is now gray
+	vis.DiscoverVertex(source)
+	cmap[source] = gray    // mark as discovered
+	queue.PushBack(source) // enqueue
+
+	// visit
+	for queue.Len() != 0 {
+		// dequeue the front element
+		// and examine it
+		elt := queue.Front()
+		queue.Remove(elt)
+		v := elt.Value.(string)
+		vis.ExamineVertex(v)
+
+		// visit neighbours
+		for _, next := range g.NextVertices(v) {
+			// leave vertex v toward vertex next
+			// and examine the edge
+			vis.ExamineEdge(v, next)
+
+			// has next vertex already been discovered
+			if cmap[next] == white {
+				// vertex has not been discovered yet
+				vis.TreeEdge(v, next)
+				vis.DiscoverVertex(next)
+				cmap[next] = gray    // mark as discovered
+				queue.PushBack(next) //enqueue
+
+				// stop if the new vertex is the target vertex
+				if next == target {
+					return
+				}
+			} else {
+				vis.NonTreeEdge(v, next)
+				if cmap[next] == gray {
+					vis.GrayTarget(v, next)
+				} else {
+					vis.BlackTarget(v, next)
+				}
+			}
+		}
+
+		// All neighbours have been found
+		// There is nothing left to do with this vertex: turn it to black
+		vis.FinishVertex(v)
+		cmap[v] = black
+	}
+}
 
 // BfsVisitorNoOp is a BfsVisitor which does nothing.
 type BfsVisitorNoOp struct{}
